b31a50c61107105fb49db65fd59977ad4ddd437d
Samson Ude <samson.ude@fbistech.com>
2026-06-07 19:30:34 +0100

    reset password

--- apps/dashboard/middleware.ts
+++ apps/dashboard/middleware.ts
@@ -5,7 +5,11 @@ export async function middleware(req: NextRequest) {
   const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET })
   const { pathname } = req.nextUrl
 
-  if (pathname.startsWith('/login') || pathname.startsWith('/vendor-onboarding')) {
+  if (
+    pathname.startsWith('/login') ||
+    pathname.startsWith('/reset-password') ||
+    pathname.startsWith('/vendor-onboarding')
+  ) {
     if (pathname.startsWith('/login') && token) return NextResponse.redirect(new URL('/', req.url))
     return NextResponse.next()
   }
